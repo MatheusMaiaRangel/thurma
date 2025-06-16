e3f77607e3feb3f7e1f8c4b8fdcce4e13f4f2729
matheusmaiarangel <matheusmaiarangel@gmail.com>
2025-06-16 10:43:49 -0300

    Reorganização da navbar na parte de editar as informações dos alunos

--- editar_aluno.php
+++ editar_aluno.php
@@ -86,8 +86,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <a class="navbar-brand" href="index.php">Thurma</a>
     <div class="collapse navbar-collapse">
         <ul class="navbar-nav me-auto">
-        <li class="nav-item"><a class="nav-link" href="cadastro.html">Cadastro</a></li>
         <li class="nav-item"><a class="nav-link" href="listagem.php">Listagem</a></li>
+        <li class="nav-item"><a class="nav-link" href="cadastro.html">Cadastro</a></li>
     </ul>
     </div>
   </div>
